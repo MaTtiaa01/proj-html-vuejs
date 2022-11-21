193032eca2f2bb2e2599d8a1cc0646d4d1305051
MaTtiaa01 <mattia2001@live.it>
2022-11-21 13:05:19 +0100

    add head_top

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -14,17 +14,43 @@ export default {
 <template>
     <header>
         <div class="head_top">
-            <div class="open_hours">
-                Open huors: {{ store.info.openHour }}
-            </div>
-            <div class="info">
-                <ul>
-                    <li><a href="#">{{ store.info.phone }}</a></li>
-                    <li><a href="#">{{ store.info.mail }}</a></li>
-                    <li><a :href="store.info.facebook"></a></li>
-                    <li><a :href="store.info.twitter"></a></li>
-                    <li><a :href="store.info.linkedin"></a></li>
-                </ul>
+            <div class="container d-flex justify-content-between">
+                <div class="open_hours d-flex align-items-center">
+                    <font-awesome-icon icon="fa-solid fa-clock" /> <span class="ms-2">Open huors: {{ store.info.openHour
+                    }}</span>
+                </div>
+                <div class="info d-flex align-items-center">
+                    <ul class="d-flex">
+                        <li>
+                            <a href="#">
+                                <font-awesome-icon icon="fa-solid fa-phone" /> <span class="me-2">{{ store.info.phone
+                                }}</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <font-awesome-icon icon="fa-solid fa-envelope" /> <span class="me-2">{{ store.info.mail
+                                }}</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a :href="store.info.facebook">
+                                <font-awesome-icon icon="fa-brands fa-facebook" />
+                            </a>
+                        </li>
+                        <li>
+                            <a :href="store.info.twitter">
+                                <font-awesome-icon icon="fa-brands fa-twitter" />
+                            </a>
+                        </li>
+                        <li>
+                            <a :href="store.info.linkedin">
+                                <font-awesome-icon icon="fa-brands fa-linkedin" />
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+
             </div>
         </div>
         <div class="head_bottom"></div>
@@ -32,5 +58,28 @@ export default {
 </template>
 
 <style lang="scss" scoped>
+@use "../assets/scss/variables.scss" as *;
 
+header {
+    .head_top {
+        background-color: $lg-dark-blue;
+        padding: 1rem 0;
+
+        .open_hours {
+            color: $lg-lighter;
+        }
+
+        .info {
+
+            ul {
+                list-style: none;
+
+                li {
+                    margin-left: 1rem;
+                }
+            }
+        }
+
+    }
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -3,10 +3,19 @@ import './style.scss'
 import App from './App.vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faFacebook } from '@fortawesome/free-brands-svg-icons'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { faPhone } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faClock } from '@fortawesome/free-solid-svg-icons'
+
+
+
+
 
 /* add icons to the library */
-library.add(faUserSecret)
+library.add(faFacebook, faTwitter, faLinkedin, faPhone, faEnvelope, faClock)
 
 createApp(App)
     .component('font-awesome-icon', FontAwesomeIcon)
